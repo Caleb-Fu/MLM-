fd9a21ec5b117466e35a9e5fbc6098c1031c72f9
c9999 <73625438+Caleb-Fu@users.noreply.github.com>
2021-06-21 03:13:47 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,50 @@
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Tokenizeration
+'''
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+tokenized_texts = [tokenizer.tokenize('[CLS]  i want to fly from boston at 838 am and arrive in denver at 1110 in the morning  [SEP]')]
+print ("Tokenize the first sentence:")
+print (tokenized_texts)
+# Set the maximum sequence length. 
+MAX_LEN = 128
+# Pad our input tokens
+'''
+Change to IDS
+'''
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+                          maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+# Use the BERT tokenizer to convert the tokens to their index numbers in the BERT vocabulary
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+print(len(input_ids[0]))
+input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+attention_masks = []
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+  seq_mask = [float(i>0) for i in seq]
+  attention_masks.append(seq_mask)
+
+print(len(attention_masks[0]))
+
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=1)
+model.cuda()
+
+
+
+
+
+
+
